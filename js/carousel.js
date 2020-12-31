ee8c55c850c30bafa5e08a5df9321e03c70befa7
Isela <isela.v.alarcon@gmail.com>
2020-12-30 21:21:28 -0600

    updating case pages

--- js/carousel.js
+++ js/carousel.js
@@ -14,12 +14,23 @@ const showSlide = index => {
   slides[currentSlide - 1].style.display = "flex"
 }
 
+const handleMenuToggle = () => {
+  // GET NAV ELEMENT
+  const nav = document.getElementById("nav-main")
+  // APPLY STYLES
+  return nav.style.display === "flex" ? (nav.style.display = "none") : (nav.style.display = "flex")
+}
+
 // BUILD ONCLICK FUNCTIONS
 const nextSlide = () => showSlide((currentSlide += 1))
 const previousSlide = () => showSlide((currentSlide -= 1))
 
 // MAKE SURE THE FUNCTION IS LOADED ON PAGE LOAD AND ACTIVATE ONCLICK EVENTS
 window.onload = () => {
+  // DETECT THE MENU BURGER ICON
+  const menuIcon = document.getElementById("nav-mobile")
+  menuIcon.addEventListener("click", () => handleMenuToggle())
+
   showSlide(currentSlide)
   document.getElementById("prev").addEventListener("click", () => previousSlide())
   document.getElementById("next").addEventListener("click", () => nextSlide())
